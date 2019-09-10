f265c01d829ed83508d001719492f7b9680fdc15
Alan Hardman <alanaktion@gmail.com>
2019-09-09 19:54:45 -0600

    Working npm build, yay not-Windows
    I actually switch to Linux instead because reasonis.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.postCss('resources/css/main.css', 'public/css', [
-    require('tailwindcss'),
-]);
+mix
+    .js('src/app.js', 'dist')
+    .postCss('src/app.css', 'dist', [
+        require('tailwindcss'),
+    ]);
